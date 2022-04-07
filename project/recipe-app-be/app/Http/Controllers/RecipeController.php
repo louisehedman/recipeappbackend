3d58152deee100c69386a2e0d21874ea7df3e3e7
louisehedman <louise.hedman@chasacademy.se>
2022-04-07 15:58:39 +0200

    update recipecontroller

--- project/recipe-app-be/app/Http/Controllers/RecipeController.php
+++ project/recipe-app-be/app/Http/Controllers/RecipeController.php
@@ -36,9 +36,10 @@ class RecipeController extends Controller {
     }
 
     public function store(Request $request, RecipeList $recipeList) {
+        $user = $recipeList->user;
 
         // check if given list belongs to current user
-        if (auth::user()) {
+        if ($user->id === auth::user()->id) {
             $validator = Validator::make($request->only('recipe_api_id', 'title', 'img'), [
                 'title' => 'required|string',
                 'recipe_api_id' => 'required|numeric',
