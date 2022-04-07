4f4f7745ffac58402424ac621d3d1c77795d1237
louisehedman <louise.hedman@chasacademy.se>
2022-04-07 13:49:59 +0200

    update

--- project/recipe-app-be/database/migrations/2022_04_02_075250_create_recipe_lists_table.php
+++ project/recipe-app-be/database/migrations/2022_04_02_075250_create_recipe_lists_table.php
@@ -16,7 +16,7 @@ class CreateRecipeListsTable extends Migration
         Schema::create('recipe_lists', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            //$table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
