d6df5bcfad711a5566007857294fdb43b809cbf6
louisehedman <louise.hedman@chasacademy.se>
2022-04-07 14:51:29 +0200

    new try

--- project/recipe-app-be/app/Models/RecipeList.php
+++ project/recipe-app-be/app/Models/RecipeList.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
-use App\Models\Recipe;
+use App\Models\RecipeId;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class RecipeList extends Model
